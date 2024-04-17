6f8908b8f788d16b02a779f3b0b0c6b3a86e0881
g4knr <85592362+g4knr@users.noreply.github.com>
2024-04-17 10:51:59 +0100

    Format inputs takes interest rates into account

--- src/utils/formatInput.ts
+++ src/utils/formatInput.ts
@@ -5,6 +5,14 @@ export function formatInput(input: Input): void {
     const { type, value } = input;
     if (type !== 'number') return;
 
+    console.log('input:');
+    console.log(input);
+
+    const { step } = input;
+    console.log(step);
+
+    if (Number(step) < 1) return;
+
     input.value = Math.round(Number(value)).toString();
   }
 }
