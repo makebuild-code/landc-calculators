7b9697a549923d6bc8d50bcbe38fe9f4172fbe5b
tim-makebuild <157041075+tim-makebuild@users.noreply.github.com>
2025-01-11 11:23:42 +0000

    pushing 24 to 12 fix

--- src/costofdoingnothing/types.d.ts
+++ src/costofdoingnothing/types.d.ts
@@ -1,9 +1,9 @@
 export interface Lender {
-    MasterLenderId: number;
-    Lender: string;
-    ResidentialRate: number;
-    BuyToLetRate: number;
-    LenderURL: string | null;
-    ResidentialRetentionProductCount: number | null;
-    BuyToLetRetentionProductCount: number | null;
+  MasterLenderId: number;
+  Lender: string;
+  ResidentialRate: number;
+  BuyToLetRate: number;
+  LenderURL: string | null;
+  ResidentialRetentionProductCount: number | null;
+  BuyToLetRetentionProductCount: number | null;
 }
