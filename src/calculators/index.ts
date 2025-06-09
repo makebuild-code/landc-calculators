f86f92596aad5df51071131f22c87633dfa1c232
g4knr <163923738+gregFaulknerMakeBuild@users.noreply.github.com>
2025-06-09 10:47:11 +0100

    tided calc index

--- src/calculators/index.ts
+++ src/calculators/index.ts
@@ -1,29 +1,26 @@
+import { queryElement } from '$utils/queryElement';
 import { queryElements } from '$utils/queryelements';
 
 import { HandleCalculator } from './handleCalculator';
 
 export const calculators = () => {
-  // Forces Outputs to Sliders - Mortgage Calc page New
-  const repaymentValueSlider = document.getElementById('RepaymentValue') as HTMLInputElement | null;
-  const depositAmountSlider = document.getElementById(
-    'DepositAmountSlider'
-  ) as HTMLInputElement | null;
-  const rateSlider = document.querySelector('[data-input="Rate"]') as HTMLInputElement | null;
-
-  if (repaymentValueSlider) {
-    repaymentValueSlider.setAttribute('data-calc-output', 'BorrowingAmountHigher');
-  }
-
-  if (depositAmountSlider) {
-    depositAmountSlider.setAttribute('data-calc-output', 'DepositAmount');
-  }
-
-  if (rateSlider) {
-    rateSlider.setAttribute('data-calc-output', 'InitialRate');
-  }
-  // ----- End
-
   const attr = 'data-calc';
+
+  // Forces Outputs to Sliders - Mortgage Calc page New
+  const repaymentValueSlider = queryElement<HTMLInputElement>(`[data-input="RepaymentValue"]`);
+  const depositAmountSlider = queryElement<HTMLInputElement>(`[data-input="DepositAmountSlider"]`);
+  const rateSlider = queryElement<HTMLInputElement>(`[data-input="Rate"]`);
+  // const repaymentValueSlider = document.getElementById('RepaymentValue') as HTMLInputElement | null;
+  // const depositAmountSlider = document.getElementById(
+  //   'DepositAmountSlider'
+  // ) as HTMLInputElement | null;
+  // const rateSlider = document.querySelector('[data-input="Rate"]') as HTMLInputElement | null;
+
+  if (repaymentValueSlider)
+    repaymentValueSlider.setAttribute(`${attr}-output`, 'BorrowingAmountHigher');
+  if (depositAmountSlider) depositAmountSlider.setAttribute(`${attr}}-output`, 'DepositAmount');
+  if (rateSlider) rateSlider.setAttribute(`${attr}}-output`, 'InitialRate');
+  // ----- End
 
   const components = queryElements<HTMLDivElement>(`[${attr}]`);
 
