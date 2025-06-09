9cb8f83eaf232d4d80543a2014ac5a5e2e822fa4
g4knr <163923738+gregFaulknerMakeBuild@users.noreply.github.com>
2025-06-09 12:19:41 +0100

    updated merge command

--- scripts/merge-into.js
+++ scripts/merge-into.js
@@ -7,7 +7,8 @@ if (!target) {
   console.error("Specify which branch to merge into (e.g. 'testing').");
   process.exit(1);
 }
+const branch = execSync('git branch --show-current').toString().trim();
 execSync(
-  `git checkout ${target} && git pull origin ${target} && git merge development && git push origin ${target}`,
+  `git checkout ${target} && git pull origin ${target} && git merge ${branch} && git push origin ${target}`,
   { stdio: 'inherit' }
 );
