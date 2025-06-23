53071c437e5bda0b3c855a12d438ee5f959f884b
g4knr <163923738+gregFaulknerMakeBuild@users.noreply.github.com>
2025-06-23 17:05:06 +0100

    Integrated LCID APi

--- src/mct/shared/api/generateLCID.ts
+++ src/mct/shared/api/generateLCID.ts
@@ -1,12 +1,22 @@
+import { MCTManager } from '../MCTManager';
+import { fetchData } from '../utils/fetchData';
+import { ENDPOINTS } from './endpoints';
+
 export const generateLCID = async (): Promise<string> => {
-  //   const respose = await fetch('/api/generate-lcid');
-  //   const data = await respose.json();
+  const response = await fetchData<{ result: { lcid: string } }>(ENDPOINTS.lcid, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      endpoint: 'NewEnquiry',
+      input: {
+        lcid: MCTManager.getLCID(),
+        icid: MCTManager.getICID(),
+        partnerId: '',
+        partnerName: '',
+      },
+    }),
+  });
 
-  //   if (!data?.lcid) throw new Error('Failed to generate LCID');
-
-  const data = {
-    lcid: '0FFA8FAE-1D74-45BE-9CDC-9FAF7783CA07',
-  };
-
-  return data.lcid;
+  if (!response?.result?.lcid) throw new Error('Failed to generate LCID');
+  return response.result.lcid;
 };
