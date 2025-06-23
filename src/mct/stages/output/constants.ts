77ce07fb7a077a3a511190b7abcc7c27eeb47891
g4knr <163923738+gregFaulknerMakeBuild@users.noreply.github.com>
2025-06-23 12:08:24 +0100

    Removed output stage

--- src/mct/stages/output/constants.ts
+++
@@ -1,6 +0,0 @@
-export const attr = {
-  component: 'data-mct-outputs',
-  element: 'data-mct-outputs-element',
-  output: 'data-mct-questions-output',
-  type: 'data-mct-questions-outputs-type',
-};
--- src/mct/stages/output/types.d.ts
+++
@@ -1,5 +0,0 @@
-export interface OutputData {
-  // We'll need to define the shape of the API response here
-  // This is a placeholder - we'll need the actual API response structure
-  [key: string]: string | number;
-}
