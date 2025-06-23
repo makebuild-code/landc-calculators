d2b39a09596b68d10de20bb84e62f67e34186819
g4knr <163923738+gregFaulknerMakeBuild@users.noreply.github.com>
2025-06-23 17:06:05 +0100

    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,8 @@ import { calculators } from './calculators';
 import { components } from './components';
 import { costOfDoingNothing } from './costofdoingnothing';
 import { mct } from './mct';
-import { testFetchProducts } from './mct/shared/api/tests/testFetchProducts';
 
 window.Webflow ||= [];
-
 window.Webflow.push(() => {
   components();
   bestbuys();
