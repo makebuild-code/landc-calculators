092df332d2384244b291762eab2a7a4e9e3c739f
g4knr <163923738+gregFaulknerMakeBuild@users.noreply.github.com>
2025-06-25 14:42:21 +0100

    Starting Results

---
+++ src/mct/stages/results/constants.ts
@@ -0,0 +1,10 @@
+export const attr = {
+  components: 'data-mct-results',
+  //   group: 'data-mct-results-group',
+  //   question: 'data-mct-results-question',
+  //   dependsOn: 'data-mct-results-depends-on',
+  //   dependsOnValue: 'data-mct-results-depends-on-value',
+  element: 'data-mct-results-element',
+  output: 'data-mct-results-output',
+  type: 'data-mct-results-outputs-type',
+};
