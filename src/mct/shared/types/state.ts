14d10bd5f9e11fe61fad774e9f4bf32e376ae426
g4knr <163923738+gregFaulknerMakeBuild@users.noreply.github.com>
2025-07-10 13:23:42 +0100

    Added Answers with keys and expected values

--- src/mct/shared/types/state.ts
+++ src/mct/shared/types/state.ts
@@ -5,11 +5,42 @@
  * and data persistence.
  */
 
+import type {
+  CreditImpairedENUM,
+  EndOfTermENUM,
+  FirstTimeBuyerENUM,
+  PurchRemoENUM,
+  ReadinessToBuyENUM,
+  RemoChangeENUM,
+  RepaymentTypeENUM,
+  ResiBtlENUM,
+  SchemePeriodsENUM,
+  SchemeTypesENUM,
+} from './api';
+
 // Answer-related types
 export type AnswerKey = string; // Name of the question group
 export type AnswerName = string; // Name of the answer input
 export type AnswerValue = string | number | (string | number)[] | null; // Value of the answer input
-export type Answers = Record<AnswerKey, AnswerValue>; // All answers
+// export type Answers = Record<AnswerKey, AnswerValue>; // All answers
+export type Answers = {
+  PurchRemo?: PurchRemoENUM; // will have by the end
+  FTB?: FirstTimeBuyerENUM;
+  ResiBtl?: ResiBtlENUM; // will have by the end
+  ReadinessToBuy?: ReadinessToBuyENUM;
+  CreditImpaired?: CreditImpairedENUM;
+  PropertyValue?: number; // will have by the end
+  DepositAmount?: number;
+  RepaymentType?: RepaymentTypeENUM; // will have by the end
+  InterestOnlyValue?: number; // optional?
+  MortgageLength?: number; // will have by the end
+  SchemeTypes?: SchemeTypesENUM[]; // will have by the end
+  SchemePeriods?: SchemePeriodsENUM; // will have by the end
+  EndOfTerm?: EndOfTermENUM;
+  RepaymentValue?: number; // will have by the end
+  RemoChange?: RemoChangeENUM;
+  Lender?: string;
+};
 export type PrefillAnswers = Record<AnswerName, AnswerValue>; // Answers that are prefilled
 
 // Calculation-related types
