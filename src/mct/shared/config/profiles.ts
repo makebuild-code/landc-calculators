1eb58fc8b12953304b022dc8ea04544e0a5dd794
g4knr <163923738+gregFaulknerMakeBuild@users.noreply.github.com>
2025-07-16 14:26:12 +0100

    Added new FTB BTL profile

--- src/mct/shared/config/profiles.ts
+++ src/mct/shared/config/profiles.ts
@@ -13,15 +13,6 @@ export const PROFILES_CONFIG: CONFIG_PROFILES = {
       },
     },
     {
-      name: ProfileNameENUM.FtbResidentialPurchase,
-      display: 'First Time Buyer - Purchase',
-      requirements: {
-        PurchRemo: 'Purchase',
-        FTB: 'Yes',
-        ResiBtl: 'Residential',
-      },
-    },
-    {
       name: ProfileNameENUM.BtlPurchase,
       display: 'Buy to Let - Purchase',
       requirements: {
@@ -31,11 +22,28 @@ export const PROFILES_CONFIG: CONFIG_PROFILES = {
       },
     },
     {
+      name: ProfileNameENUM.FtbResidentialPurchase,
+      display: 'First Time Buyer - Purchase',
+      requirements: {
+        PurchRemo: 'Purchase',
+        FTB: 'Yes',
+        ResiBtl: 'Residential',
+      },
+    },
+    {
+      name: ProfileNameENUM.FtbBtlPurchase,
+      display: 'First Time Buyer - Buy to Let - Purchase',
+      requirements: {
+        PurchRemo: 'Purchase',
+        FTB: 'Yes',
+        ResiBtl: 'Btl',
+      },
+    },
+    {
       name: ProfileNameENUM.ResidentialRemortgage,
       display: 'Residential - Remortgage',
       requirements: {
         PurchRemo: 'Remortgage',
-        // FTB: 'No',
         ResiBtl: 'Residential',
       },
     },
@@ -44,7 +52,6 @@ export const PROFILES_CONFIG: CONFIG_PROFILES = {
       display: 'Buy to Let - Remortgage',
       requirements: {
         PurchRemo: 'Remortgage',
-        // FTB: 'No',
         ResiBtl: 'Btl',
       },
     },
