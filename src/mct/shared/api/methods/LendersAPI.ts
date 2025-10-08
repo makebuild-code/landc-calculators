93759412bba150ce727c83258370a5ddb8ed8d33
g4knr <contact@g4knr.co.uk>
2025-10-08 15:45:31 +0100

    Added caching for the Lenders API

--- src/mct/shared/api/methods/LendersAPI.ts
+++ src/mct/shared/api/methods/LendersAPI.ts
@@ -3,10 +3,21 @@ import { API_CONFIG } from '$mct/config';
 import type { LenderDetails, LenderListResponse } from '$mct/types';
 
 export class LendersAPI {
+  private lenders: LenderDetails[] = [];
+
   constructor(private client: APIClient) {}
 
   async getAll(): Promise<LenderDetails[]> {
     const response = await this.client.request<LenderListResponse>(API_CONFIG.endpoints.lenders);
+    this.lenders = response.result.lenders;
     return response.result.lenders;
+  }
+
+  async getFilteredAndSorted(): Promise<LenderDetails[]> {
+    if (this.lenders.length === 0) await this.getAll();
+    const response = this.lenders
+      .filter((lender) => lender.LenderName !== '' && lender.LenderName !== null)
+      .sort((a, b) => a.LenderName.localeCompare(b.LenderName));
+    return response;
   }
 }
