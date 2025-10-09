285ce7382f12a12d0dbaa8f5fc3edc49c4a5c23e
g4knr <contact@g4knr.co.uk>
2025-10-09 10:56:34 +0100

    Updated type for NewBuild

--- src/mct/shared/config/filters.ts
+++ src/mct/shared/config/filters.ts
@@ -2,7 +2,7 @@ import { SapValueENUM, type CONFIG_FILTERS } from '$mct/types';
 import { getEnumKey } from '../utils/common/getEnumKey';
 
 export const FILTERS_CONFIG: CONFIG_FILTERS = {
-  NewBuild: 'false',
+  NewBuild: false,
   SapValue: getEnumKey(SapValueENUM, SapValueENUM.No),
   ShowSharedOwnership: true,
 };
