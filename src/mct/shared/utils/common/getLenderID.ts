fc8244204cdbec6789917546bd50841bf57c41b9
g4knr <contact@g4knr.co.uk>
2025-10-09 08:24:18 +0100

    Function to get given LenderID from string

--- src/mct/shared/utils/common/getLenderID.ts
+++ src/mct/shared/utils/common/getLenderID.ts
@@ -1,4 +1,5 @@
 export const getLenderID = (lenderString: string, type: 'master' | 'residential' | 'btl'): string | undefined => {
+  if (!lenderString) return undefined;
   // regex to get the master lender id, residential lender id, and btl lender id from the lender id given the format MasterLenderID:123|ResidentialLenderID:456|BTLLenderID:789
   const regex = /MasterLenderID:(\d+)\|ResidentialLenderID:(\d+)\|BTLLenderID:(\d+)/;
   const match = lenderString.match(regex);
